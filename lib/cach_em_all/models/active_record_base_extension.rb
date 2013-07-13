632daab63f2b524138bb964b5a72dd4b39b42866
Oscar Esgalha <oscaresgalha@gmail.com>
2013-07-13 14:44:32 -0300

    Renamed all_cache_key to cache_key to use with the cache helper

--- lib/cach_em_all/models/active_record_base_extension.rb
+++ lib/cach_em_all/models/active_record_base_extension.rb
@@ -2,7 +2,7 @@ module CachEmAll
   module ActiveRecordBaseExtension
     extend ActiveSupport::Concern
     included do
-      def self.all_cache_key
+      def self.cache_key
         count          = self.count
         max_updated_at = self.maximum(:updated_at).try(:utc).try(:to_s, :number)
         name = self.name.tableize.pluralize
